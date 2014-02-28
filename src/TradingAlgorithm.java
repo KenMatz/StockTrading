140d0f9caa20076fc66aedfd27dd8f6d5e3d4531
Quinn Liu <quinnliu@vt.edu>
2014-02-28 18:51:59 -0500

    adding helpful tips

---
+++ src/TradingAlgorithm.java
@@ -0,0 +1,18 @@
+public class TradingAlgorithm {
+
+    public TradingAlgorithm() {
+
+    }
+
+    public void startTrading() {
+
+    }
+
+    public String nextBestCommand() {
+	// The key to winning is to make the most
+	// money possible from dividends. It is the primary
+	// way to increase your net worth.
+
+	return "";
+    }
+}
