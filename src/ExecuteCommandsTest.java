614bab37a50babc47450002a6258174ae6e4b50f
Quinn Liu <quinnliu@vt.edu>
2014-02-28 18:45:16 -0500

    a junit test

---
+++ src/ExecuteCommandsTest.java
@@ -0,0 +1,10 @@
+public class ExecuteCommandsTest extends junit.framework.TestCase {
+
+    public void setUp() {
+
+    }
+
+    public void testCallingAPICommands() {
+	System.out.println("this is working");
+    }
+}
