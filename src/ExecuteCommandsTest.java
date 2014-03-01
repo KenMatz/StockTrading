acbc7af33e10de2e50fcb0dc7da46fa38af54025
Quinn Liu <quinnliu@vt.edu>
2014-02-28 19:40:21 -0500

    setup for running the trading algorithm for a set amount of time

--- src/ExecuteCommandsTest.java
+++ src/ExecuteCommandsTest.java
@@ -1,24 +1,31 @@
 import java.io.IOException;
 
 /**
- *  @author Quinn Liu (quinnliu@vt.edu)
- *  @version Feb 28, 2014
+ * @author Quinn Liu (quinnliu@vt.edu)
+ * @version Feb 28, 2014
  */
 public class ExecuteCommandsTest extends junit.framework.TestCase {
 
-    public void setUp() {
+    private long numberOfSecondToRunTradingAlgorithm;
 
+    public void setUp() {
+	this.numberOfSecondToRunTradingAlgorithm = 5;
     }
 
     public void testCallingAPICommands() throws IOException {
+	long currentTimeInSecondsSinceMidnightJanuaryFirst1970 = (System
+		.currentTimeMillis() / 1000);
+	long timeToStopProgram = currentTimeInSecondsSinceMidnightJanuaryFirst1970
+		+ this.numberOfSecondToRunTradingAlgorithm;
+
 	System.out.println("this is working");
 
 	String[] commandLineArguments = { "172.31.231.85", "17429",
 		"onesqueakywheel", "onemanarmy", "MY_CASH" };
 	ExchangeClient.main(commandLineArguments);
 
-	long currentTimeInSecondsSinceMidnightJanuaryFirst1970 = (System
-		.currentTimeMillis() / 1000);
-	System.out.println(currentTimeInSecondsSinceMidnightJanuaryFirst1970);
+	System.out
+		.println(currentTimeInSecondsSinceMidnightJanuaryFirst1970);
+	System.out.println(timeToStopProgram);
     }
 }
