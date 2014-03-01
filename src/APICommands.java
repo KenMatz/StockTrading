68a9f3726bdb2e9ee487eb5230c10bd703985b2e
Quinn Liu <quinnliu@vt.edu>
2014-02-28 20:18:19 -0500

    creating great method names for api commands

--- src/APICommands.java
+++ src/APICommands.java
@@ -3,12 +3,6 @@ public class APICommands {
     public APICommands() {
 	// not necessary
 
-	// MY_CASH = output your current cash
-	// MY_SECURITIES = output all shares you own
-	// MY_OFFERS = output all current offers BID & ASK on the exchange
-	// SECURITIES = output all securities in the form <ticker> <net_worth>
-	// <dividend> <volatility>
-
 	// OFFERS <ticker> = will list all offers on the exchange for a ticker
 	// BID <ticker> <price> <shares> = place a new bid
 	// ASK <ticker> <price> <shares> = place a new ask
@@ -17,6 +11,22 @@ public class APICommands {
 
     public String getCurrentCash() {
 	return "MY_CASH";
+    }
+
+    public String getAllSharesYouOwn() {
+	return "MY_SECURITIES";
+    }
+
+    public String getAllCurrentOffersBIDandASKonTheExchange() {
+	return "MY_OFFERS";
+    }
+
+    public String getAllSecuritiesInTheForm_ticker_netWorth_divident_volatility() {
+	return "SECURITIES";
+    }
+
+    public String getListAllOffersOnTheExchangeForATicker(int ticker) {
+	return "OFFERS " + ticker;
     }
 
 }
