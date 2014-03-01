91a6b6fb919f42a1770d395a2f18fc734ae29470
Quinn Liu <quinnliu@vt.edu>
2014-02-28 19:08:35 -0500

    added initial commands

--- src/TradingAlgorithm.java
+++ src/TradingAlgorithm.java
@@ -8,11 +8,29 @@ public class TradingAlgorithm {
 
     }
 
+    /**
+     * The key to winning is to make the most money possible from dividends. It
+     * is the primary way to increase your net worth.
+     *
+     * Because the divident payout per period is a geometric series, the toal
+     * divident payout for holding a stock is capped. We greatly encourage
+     * cooperation with other teams as you can help each other trading the
+     * stocks you do not own back and forth.
+     *
+     * Remember, the shorter time you own a security the higher its average
+     * payout per second.
+     */
     public String nextBestCommand() {
-	// The key to winning is to make the most
-	// money possible from dividends. It is the primary
-	// way to increase your net worth.
+	// MY_CASH = output your current cash
+	// MY_SECURITIES = output all shares you own
+	// MY_OFFERS = output all current offers BID & ASK on the exchange
+	// SECURITIES = output all securities in the form <ticker> <net_worth>
+	// <dividend> <volatility>
 
+	// OFFERS <ticker> = will list all offers on the exchange for a ticker
+	// BID <ticker> <price> <shares> = place a new bid
+	// ASK <ticker> <price> <shares> = place a new ask
+	// CLEAR_BID <ticker> = clear your bid
 	return "";
     }
 }
