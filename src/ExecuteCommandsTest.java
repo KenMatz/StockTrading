45c6f886d48d7ade2a5be938caa7b21325c92ead
Quinn Liu <quinnliu@vt.edu>
2014-02-28 19:19:22 -0500

    program to run automatically as a test

--- src/ExecuteCommandsTest.java
+++ src/ExecuteCommandsTest.java
@@ -1,10 +1,16 @@
+import java.io.IOException;
+
 public class ExecuteCommandsTest extends junit.framework.TestCase {
 
     public void setUp() {
 
     }
 
-    public void testCallingAPICommands() {
+    public void testCallingAPICommands() throws IOException {
 	System.out.println("this is working");
+
+	String[] commandLineArguments = { "172.31.231.85", "17429",
+		"onesqueakywheel", "onemanarmy", "MY_CASH" };
+	ExchangeClient.main(commandLineArguments);
     }
 }
